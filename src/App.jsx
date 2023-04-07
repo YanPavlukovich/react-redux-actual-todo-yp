acdcfeff87f171cde1f02157f4bc5b60b4bbedb3
YanPavlukovich <filin022@gmail.com>
2023-04-07 18:35:22 +0300

    наладили работу диспетчера через хуки useDispatch createSlice

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 import InputField from "./components/InputField";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./store/todoSlice.js";
 
 function App() {
-  const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
-
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        },
-      ]);
-      setText("");
-    }
-  };
-
-  const removeTodo = (toodId) => {
-    setTodos(todos.filter((todos) => todos.id !== toodId));
-  };
-
-  const toggleTodoComplete = (toodId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== toodId) {
-          return todo;
-        }
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      })
-    );
+  const dispatch = useDispatch();
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
   };
 
   return (
     <div className="App">
-      <InputField text={text} handleInput={setText} handleSubmit={addTodo} />
-      <TodoList
-        todos={todos}
-        toggleTodoComplete={toggleTodoComplete}
-        removeTodo={removeTodo}
-      />
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <TodoList />
     </div>
   );
 }
